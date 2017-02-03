c23b011a682b2d25fafb00a33d40f0c524ae7540
Paul Rouget <me@paulrouget.com>
2017-02-03 09:27:53 +0100

    clicking

--- src/events.rs
+++ src/events.rs
@@ -1,4 +1,5 @@
 use servo::compositing::windowing::WindowEvent;
+use servo::compositing::windowing::MouseWindowEvent;
 use webrender_traits;
 use glutin;
 use std::cell::{Cell, RefCell};
@@ -12,6 +13,8 @@ const LINE_HEIGHT: f32 = 38.0;
 pub struct GlutinEventHandler {
     events_for_servo: RefCell<Vec<WindowEvent>>,
     mouse_pos: Cell<Point2D<i32>>,
+    mouse_down_button: Cell<Option<glutin::MouseButton>>,
+    mouse_down_point: Cell<Point2D<i32>>,
 }
 
 impl GlutinEventHandler {
@@ -19,6 +22,8 @@ impl GlutinEventHandler {
         GlutinEventHandler {
             events_for_servo: RefCell::new(vec!()),
             mouse_pos: Cell::new(Point2D::new(0, 0)),
+            mouse_down_button: Cell::new(None),
+            mouse_down_point: Cell::new(Point2D::new(0, 0)),
         }
     }
 
@@ -54,6 +59,41 @@ impl GlutinEventHandler {
                 let event = WindowEvent::Scroll(scroll_location, TypedPoint2D::new(mouse_pos.x as i32, mouse_pos.y as i32), phase);
                 self.events_for_servo.borrow_mut().push(event);
             },
+            glutin::Event::MouseInput(element_state, mouse_button) => {
+                if mouse_button == glutin::MouseButton::Left || mouse_button == glutin::MouseButton::Right {
+                    let mouse_pos = self.mouse_pos.get();
+                    use servo::script_traits::MouseButton;
+                    let (x, y) = (mouse_pos.x, mouse_pos.y);
+                    // FIXME(tkuehn): max pixel dist should be based on pixel density
+                    let max_pixel_dist = 10f64;
+                    let event = match element_state {
+                        glutin::ElementState::Pressed => {
+                            self.mouse_down_point.set(Point2D::new(x, y));
+                            self.mouse_down_button.set(Some(mouse_button));
+                            MouseWindowEvent::MouseDown(MouseButton::Left, TypedPoint2D::new(x as f32, y as f32))
+                        }
+                        glutin::ElementState::Released => {
+                         let mouse_up_event = MouseWindowEvent::MouseUp(MouseButton::Left, TypedPoint2D::new(x as f32, y as f32));
+                            match self.mouse_down_button.get() {
+                                None => mouse_up_event,
+                                Some(but) if mouse_button == but => {
+                                    let pixel_dist = self.mouse_down_point.get() - Point2D::new(x, y);
+                                    let pixel_dist = ((pixel_dist.x * pixel_dist.x +
+                                                       pixel_dist.y * pixel_dist.y) as f64).sqrt();
+                                    if pixel_dist < max_pixel_dist {
+                                        self.events_for_servo.borrow_mut().push(WindowEvent::MouseWindowEventClass(mouse_up_event));
+                                        MouseWindowEvent::Click(MouseButton::Left, TypedPoint2D::new(x as f32, y as f32))
+                                    } else {
+                                        mouse_up_event
+                                    }
+                                },
+                                Some(_) => mouse_up_event,
+                            }
+                        }
+                    };
+                    self.events_for_servo.borrow_mut().push(WindowEvent::MouseWindowEventClass(event));
+                }
+            }
             glutin::Event::Closed => {
                 return true
             }
@@ -69,5 +109,4 @@ impl GlutinEventHandler {
         events.extend(mem::replace(&mut *self.events_for_servo.borrow_mut(), Vec::new()).into_iter());
         events
     }
-
 }
