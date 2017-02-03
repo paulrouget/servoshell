da436f004aacb58dbd43382e07e57b9ec5063f1b
Paul Rouget <me@paulrouget.com>
2017-02-03 08:59:45 +0100

    wip

--- src/events.rs
+++ src/events.rs
@@ -3,6 +3,9 @@ use webrender_traits;
 use glutin;
 use std::cell::{Cell, RefCell};
 use euclid::{Point2D, TypedPoint2D};
+use servo::script_traits::TouchEventType;
+use glutin::TouchPhase;
+
 
 const LINE_HEIGHT: f32 = 38.0;
 
@@ -26,6 +29,31 @@ impl GlutinEventHandler {
                 let event = WindowEvent::MouseWindowMoveEventClass(TypedPoint2D::new(x as f32, y as f32));
                 self.events_for_servo.borrow_mut().push(event);
             }
+            glutin::Event::MouseWheel(delta, phase) => {
+                let (dx, dy) = match delta {
+                    glutin::MouseScrollDelta::LineDelta(dx, dy) => (dx, dy * LINE_HEIGHT),
+                    glutin::MouseScrollDelta::PixelDelta(dx, dy) => (dx, dy),
+                };
+                let scroll_location = webrender_traits::ScrollLocation::Delta(TypedPoint2D::new(dx, dy));
+                let phase = match phase {
+                    TouchPhase::Started => TouchEventType::Down,
+                    TouchPhase::Moved => TouchEventType::Move,
+                    TouchPhase::Ended => TouchEventType::Up,
+                    TouchPhase::Cancelled => TouchEventType::Cancel,
+                };
+
+                if let webrender_traits::ScrollLocation::Delta(mut delta) = scroll_location {
+                    if delta.y.abs() >= delta.x.abs() {
+                        delta.x = 0.0;
+                    } else {
+                        delta.y = 0.0;
+                    }
+                }
+
+                let mouse_pos = self.mouse_pos.get();
+                let event = WindowEvent::Scroll(scroll_location, TypedPoint2D::new(mouse_pos.x as i32, mouse_pos.y as i32), phase);
+                self.events_for_servo.borrow_mut().push(event);
+            },
             glutin::Event::Closed => {
                 return true
             }
