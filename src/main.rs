323020453872cc79a1cd7d2453cad420af00fc17
Paul Rouget <me@paulrouget.com>
2017-02-03 07:09:09 +0100

    basic events forwarding

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,14 @@ extern crate objc;
 extern crate servo;
 extern crate servo_geometry;
 extern crate style_traits;
+extern crate webrender_traits;
+extern crate euclid;
 
 mod ui;
 mod shell_window;
+mod events;
 
+use events::GlutinEventHandler;
 use std::rc::Rc;
 use servo::config::servo_version;
 use servo::compositing::windowing::WindowEvent;
@@ -33,16 +37,20 @@ fn main() {
     // Pipeline creation fails is layout_threads pref not set
     PREFS.set("layout.threads", PrefValue::Number(1.0));
 
-    let w = Rc::new(ShellWindow::new());
+    let shell_window = Rc::new(ShellWindow::new());
 
-    let mut browser = servo::Browser::new(w.clone());
+    let glutin_event_handler = GlutinEventHandler::new();
+
+    let mut browser = servo::Browser::new(shell_window.clone());
     browser.handle_events(vec![WindowEvent::InitializeCompositing]);
+
     loop {
-        w.glutin_window().wait_events().next();
-        // FIXME: translate glutin event to Servo event
-        // let glutin_event = w.glutin_window.wait_events().next();
-        // match glutin_event {
-        // }
-        browser.handle_events(vec![WindowEvent::Refresh]);
+        let glutin_event = shell_window.glutin_window().wait_events().next();
+        let closed = glutin_event_handler.handle_glutin_event(glutin_event.unwrap());
+        if closed {
+            // FIXME
+        }
+        let servo_events = glutin_event_handler.get_events_for_servo();
+        browser.handle_events(servo_events);
     }
 }
